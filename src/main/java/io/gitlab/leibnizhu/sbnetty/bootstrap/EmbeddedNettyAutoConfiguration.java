dfa891d4c34c0fca6654f0d309099a89551ce174
Leibniz.Hu <leibnizhu@gmail.com>
2017-08-24 12:27:52 +0800

    增加注释

--- src/main/java/io/gitlab/leibnizhu/sbnetty/bootstrap/EmbeddedNettyAutoConfiguration.java
+++ src/main/java/io/gitlab/leibnizhu/sbnetty/bootstrap/EmbeddedNettyAutoConfiguration.java
@@ -11,7 +11,12 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 /**
- * @author Leibniz
+ * 配置加载内置Netty容器的工厂类Bean。
+ * 最早是直接将EmbeddedNettyFactory加@Component注解，这样集成在任何环境中都会加载，可能引起端口冲突。
+ * 所以通过这个配置类，配置在当前上下文缺少EmbeddedServletContainerFactory接口实现类时（即缺少内置Servlet容器），加载EmbeddedNettyFactory
+ * 这样SpringBoot项目在引入这个maven依赖，并且排除了内置tomcat依赖、且没引入其他servlet容器（如jetty）时，就可以通过工厂类加载并启动netty容器了。
+ *
+ * @author Leibniz 2017-08-24
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Configuration
