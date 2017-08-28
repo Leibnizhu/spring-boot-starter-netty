a517132ae44cc346642ae04891d8f5bd0554dbbe
Leibniz.Hu <leibnizhu@gmail.com>
2017-08-28 14:10:23 +0800

    实现请求的解析

---
+++ src/main/java/io/gitlab/leibnizhu/sbnetty/request/HttpRequestInputStream.java
@@ -0,0 +1,211 @@
+package io.gitlab.leibnizhu.sbnetty.request;
+
+import com.google.common.primitives.Ints;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.LastHttpContent;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * 将Netty的http 解码结果解析成Servlet支持的
+ */
+public class HttpRequestInputStream extends ServletInputStream {
+    private final Channel channel; //netty ChannelHandlerContext的channel
+    private AtomicBoolean closed; //输入流是否已经关闭，保证线程安全
+    private final BlockingQueue<HttpContent> queue; //HttpContent的队列，一次请求可能有多次加入
+    private HttpContent current;
+    private int currentLength;
+    private ReadListener readListener;
+
+    public HttpRequestInputStream(Channel channel) {
+        this.channel = checkNotNull(channel);
+        this.closed = new AtomicBoolean();
+        queue = new LinkedBlockingQueue<>();
+    }
+
+    public void addContent(HttpContent httpContent) {
+        checkNotClosed();
+        queue.offer(httpContent.retain());
+    }
+
+    public int getCurrentLength() {
+        return currentLength;
+    }
+
+    @Override
+    public int readLine(byte[] b, int off, int len) throws IOException {
+        checkNotNull(b);
+        return super.readLine(b, off, len); //模板方法，会调用当前类实现的read()方法
+    }
+
+    /**
+     * 本次请求没再有新的HttpContent输入，而且当前的内容全部被读完
+     * @return true=读取完毕 反之false
+     */
+    @Override
+    public boolean isFinished() {
+        checkNotClosed();
+        return isLastContent() && current.content().readableBytes() == 0;
+    }
+
+    /**
+     * 已经传入本次请求所有HttpContent
+     */
+    private boolean isLastContent() {
+        return current instanceof LastHttpContent;
+    }
+
+    /**
+     * 已读入至少一次HttpContent且未读取完所有内容，或者HttpContent队列非空
+     */
+    @Override
+    public boolean isReady() {
+        checkNotClosed();
+        return (current != null && current.content().readableBytes() > 0) || !queue.isEmpty();
+    }
+
+    @Override
+    public void setReadListener(ReadListener readListener) {
+        checkNotClosed();
+        checkNotNull(readListener);
+        this.readListener = readListener;
+    }
+
+    /**
+     * 跳过n个字节
+     */
+    @Override
+    public long skip(long n) throws IOException {
+        checkNotClosed();
+        ByteBuf content = current.content();
+        long skipLen = Math.min(content.readableBytes(), n); //实际可以跳过的字节数
+        content.skipBytes(Ints.checkedCast(skipLen));
+        return skipLen;
+    }
+
+    /**
+     * @return 可读字节数
+     */
+    @Override
+    public int available() throws IOException {
+        return null == current ? 0 : current.content().readableBytes();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (closed.compareAndSet(false, true)) {
+            closeHttpContentQueue();
+            closeCurrentHttpContent();
+        }
+    }
+
+    /**
+     * 关闭当前HttpContent
+     */
+    private void closeCurrentHttpContent() {
+        if(current != null){
+            current.release();
+            current = null;
+        }
+    }
+
+    /**
+     * 关闭HttpContent队列
+     */
+    private void closeHttpContentQueue() {
+        while(!queue.isEmpty()){
+            HttpContent content = queue.poll();
+            if(content != null){
+                content.release();
+            }
+        }
+        queue.clear();
+    }
+
+    /**
+     * 尝试更新current，然后读取len个字节并复制到b中（off下标开始）
+     * @return 实际读取的字节数
+     */
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        checkNotNull(b);
+        if (0 == len) {
+            return 0;
+        }
+        poll();
+        if (isFinished()) {
+            return -1;
+        }
+        ByteBuf byteBuf = readContent(len);//读取len个字节
+        int readableBytes = byteBuf.readableBytes();
+        byteBuf.readBytes(b, off, readableBytes);//复制到b
+        return readableBytes - byteBuf.readableBytes();//返回实际读取的字节数
+    }
+
+    /**
+     * 尝试更新current，然后读取一个字节，并返回
+     */
+    @Override
+    public int read() throws IOException {
+        poll();
+        if (isFinished()) {
+            return -1;
+        }
+        return readContent(1).getByte(0);
+    }
+
+    /**
+     * 从current的HttpContent中读取length个字节
+     */
+    private ByteBuf readContent(int length) {
+        ByteBuf content = current.content();
+        if (length < content.readableBytes()) {
+            return content.readSlice(length);
+        } else {
+            return content;
+        }
+    }
+
+    /**
+     * 如果没有可读字节了，从HttpContent队列中获取一个到current中
+     * 如果readListener非空，则非阻塞，读不到数据也直接返回
+     * @throws IOException channel非激活状态
+     */
+    private void poll() throws IOException {
+        checkNotClosed();
+        if (null == current || current.content().readableBytes() == 0) {
+            boolean blocking = null == readListener;
+            while (!isLastContent()) { //current为空，或者current不是当前请求最后一个HttpContent
+                try {
+                    current = queue.poll(1000, TimeUnit.NANOSECONDS);
+                } catch (InterruptedException ignored) {
+                }
+                if(current != null){
+                    this.currentLength = current.content().readableBytes();
+                }
+                if (current != null || !blocking) { //队列中读取到数据，或者readListener非空（非阻塞），则退出
+                    break;
+                }
+                if (!channel.isActive()) {
+                    throw new IOException("Channel is not active");
+                }
+            }
+        }
+    }
+
+    private void checkNotClosed() {
+        if (closed.get()) {
+            throw new IllegalStateException("Stream is closed");
+        }
+    }
+}
---
+++ src/main/java/io/gitlab/leibnizhu/sbnetty/request/NettyHttpServletRequest.java
@@ -0,0 +1,605 @@
+package io.gitlab.leibnizhu.sbnetty.request;
+
+import com.google.common.primitives.Ints;
+import io.gitlab.leibnizhu.sbnetty.core.NettyAsyncContext;
+import io.gitlab.leibnizhu.sbnetty.core.NettyContext;
+import io.gitlab.leibnizhu.sbnetty.core.NettyRequestDispatcher;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.security.Principal;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Leibniz
+ */
+public class NettyHttpServletRequest implements HttpServletRequest {
+    public static final String DISPATCHER_TYPE = NettyRequestDispatcher.class.getName() + ".DISPATCHER_TYPE";
+
+    private final ChannelHandlerContext ctx;
+    private final NettyContext servletContext;
+    private final HttpRequest request;
+    private final HttpRequestInputStream inputStream;
+
+    private boolean asyncSupported = true;
+    private NettyAsyncContext asyncContext;
+    private HttpServletResponse servletResponse;
+
+    public NettyHttpServletRequest(ChannelHandlerContext ctx, NettyContext servletContext, HttpRequest request, HttpServletResponse servletResponse, HttpRequestInputStream inputStream) {
+        this.ctx = ctx;
+        this.servletContext = servletContext;
+        this.request = request;
+        this.servletResponse = servletResponse;
+        this.inputStream = inputStream;
+        this.attributes = new ConcurrentHashMap<>();
+        this.headers = request.headers();
+        //处理servletPath
+        String servletPath = request.uri().replace(servletContext.getContextPath(), "");
+        if (!servletPath.startsWith("/")) {
+            servletPath = "/" + servletPath;
+        }
+        int queryInx = servletPath.indexOf('?');
+        if (queryInx > -1) {
+            this.queryString = servletPath.substring(queryInx + 1, servletPath.length());
+            servletPath = servletPath.substring(0, queryInx);
+        }
+        this.servletPath = servletPath;
+        this.requestUri = this.servletContext.getContextPath() + servletPath; //TODO 加上pathInfo
+    }
+
+    @SuppressWarnings("unused")
+    HttpRequest getNettyRequest() {
+        return request;
+    }
+
+
+    /*====== Cookie 相关方法 开始 ======*/
+    private Cookie[] cookies;
+    private transient boolean cookieParsed = false;
+
+    @Override
+    public Cookie[] getCookies() {
+        if (!cookieParsed) {
+            parseCookie();
+        }
+        return cookies;
+    }
+
+    /**
+     * 解析request中的Cookie到本类的cookies数组中
+     *
+     * @author Leibniz
+     */
+    private void parseCookie() {
+        if (cookieParsed) {
+            return;
+        }
+
+        Set<io.netty.handler.codec.http.cookie.Cookie> nettyCookies = ServerCookieDecoder.LAX.decode(this.headers.get("Cookie"));
+        if (nettyCookies.size() == 0) {
+            return;
+        }
+        this.cookies = new Cookie[nettyCookies.size()];
+        Iterator<io.netty.handler.codec.http.cookie.Cookie> itr = nettyCookies.iterator();
+        int i = 0;
+        while (itr.hasNext()) {
+            io.netty.handler.codec.http.cookie.Cookie nettyCookie = itr.next();
+            Cookie servletCookie = new Cookie(nettyCookie.name(), nettyCookie.value());
+            servletCookie.setMaxAge(Ints.checkedCast(nettyCookie.maxAge()));
+            servletCookie.setDomain(nettyCookie.domain());
+            servletCookie.setPath(nettyCookie.path());
+            servletCookie.setHttpOnly(nettyCookie.isHttpOnly());
+            this.cookies[i++] = servletCookie;
+        }
+
+        this.cookieParsed = true;
+    }
+
+    /*====== Cookie 相关方法 结束 ======*/
+
+
+    /*====== Header 相关方法 开始 ======*/
+    private HttpHeaders headers;
+
+    @Override
+    public long getDateHeader(String name) {
+        return this.headers.getTimeMillis(name);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return this.headers.get(name);
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        return Collections.enumeration(this.headers.getAll(name));
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return Collections.enumeration(this.headers.names());
+    }
+
+    @Override
+    public int getIntHeader(String name) {
+        String headerStringValue = this.headers.get(name);
+        if (headerStringValue == null) {
+            return -1;
+        }
+        return Integer.parseInt(headerStringValue);
+    }
+    /*====== Header 相关方法 结束 ======*/
+
+
+    /*====== 各种路径 相关方法 开始 ======*/
+    private String servletPath;
+    private String queryString;
+    private String pathInfo;
+    private String requestUri;
+
+    @Override
+    public String getMethod() {
+        return request.method().name();
+    }
+
+    //TODO ServletPath和PathInfo应该是互补的，根据URL-Pattern匹配的路径不同而不同
+    // 现在把PathInfo恒为null，ServletPath恒为uri-contextPath
+    // 可以满足SpringBoot的需求，但不满足ServletPath和PathInfo的语义
+    // 需要在RequestUrlPatternMapper匹配的时候设置,new NettyRequestDispatcher的时候传入MapperData
+    @Override
+    public String getPathInfo() {
+        return this.pathInfo;
+    }
+
+    @Override
+    public String getContextPath() {
+        return servletContext.getContextPath();
+    }
+
+    @Override
+    public String getQueryString() {
+        return this.queryString;
+    }
+
+    @Override
+    public String getRequestURI() {
+        return this.requestUri;
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return null;
+    }
+
+    @Override
+    public String getServletPath() {
+        return this.servletPath;
+    }
+
+    @Override
+    @Deprecated
+    public String getRealPath(String path) {
+        return null;
+    }
+     /*====== 各种路径 相关方法 结束 ======*/
+
+
+    /*====== Session 相关方法 开始 ======*/
+    //TODO Session处理
+    @Override
+    public HttpSession getSession(boolean create) {
+        return null;
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return null;
+    }
+
+    @Override
+    public String changeSessionId() {
+        return null;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        return false;
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        return false;
+    }
+
+    @Override
+    @Deprecated
+    public boolean isRequestedSessionIdFromUrl() {
+        return isRequestedSessionIdFromURL();
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        return null;
+    }
+    /*====== Session 相关方法 结束 ======*/
+
+
+
+    /*====== Request Parameters 相关方法 开始 ======*/
+
+    private transient boolean isParameterParsed = false; //请求参数是否已经解析
+
+    private Map<String, String[]> paramMap = new HashMap<>(); //存储请求参数
+
+    /**
+     * 解析请求参数
+     */
+    private void parseParameter() {
+        if (isParameterParsed) {
+            return;
+        }
+
+        QueryStringDecoder queryStringDecoder = new QueryStringDecoder(request.uri());
+        Map<String, List<String>> params = queryStringDecoder.parameters();
+        for (Map.Entry<String, List<String>> entry : params.entrySet()) {
+            List<String> valueList = entry.getValue();
+            String[] valueArray = new String[valueList.size()];
+            paramMap.put(entry.getKey(), valueList.toArray(valueArray));
+        }
+
+        this.isParameterParsed = true;
+    }
+
+    /**
+     * 检查请求参数是否已经解析。
+     * 如果还没有则解析之。
+     */
+    private void checkParameterParsed() {
+        if (!isParameterParsed) {
+            parseParameter();
+        }
+    }
+
+    @Override
+    public String getParameter(String name) {
+        checkParameterParsed();
+        String[] values = paramMap.get(name);
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        return values[0];
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        checkParameterParsed();
+        return Collections.enumeration(paramMap.keySet());
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        checkParameterParsed();
+        return paramMap.get(name);
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        checkParameterParsed();
+        return paramMap;
+    }
+
+    /*====== Request Parameters 相关方法 结束 ======*/
+
+
+    /*====== 请求协议、地址、端口 相关方法 开始 ======*/
+    @Override
+    public String getProtocol() {
+        return request.protocolVersion().toString();
+    }
+
+    @Override
+    public String getScheme() {
+        return request.protocolVersion().protocolName();
+    }
+
+    private InetSocketAddress socketAddress; //请求的服务地址
+    private transient boolean isServerParsed = false; //请求服务地址是否已经解析过
+
+    private void checkAndParsedServer() {
+        if (isServerParsed) {
+            return;
+        }
+        String hostHeader = headers.get("Host");
+        if (hostHeader != null) {
+            String[] parsed = hostHeader.split(":");
+            if (parsed.length > 1) {
+                socketAddress = new InetSocketAddress(parsed[0], Integer.parseInt(parsed[1]));
+            } else {
+                socketAddress = new InetSocketAddress(parsed[0], 80);
+            }
+        }
+        isServerParsed = true;
+    }
+
+    @Override
+    public String getServerName() {
+        checkAndParsedServer();
+        return socketAddress.getHostName();
+    }
+
+    @Override
+    public int getServerPort() {
+        checkAndParsedServer();
+        return socketAddress.getPort();
+    }
+
+    @Override
+    public String getLocalName() {
+        return "localhost";
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return "127.0.0.1";
+    }
+
+    @Override
+    public int getLocalPort() {
+        return 0;
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return ((InetSocketAddress) ctx.channel().remoteAddress()).getAddress().getHostAddress();
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return ((InetSocketAddress) ctx.channel().remoteAddress()).getHostName();
+    }
+
+    @Override
+    public int getRemotePort() {
+        return ((InetSocketAddress) ctx.channel().remoteAddress()).getPort();
+    }
+
+    /*====== 请求协议、地址、端口 相关方法 结束 ======*/
+
+
+    /*====== Request Attributes 相关方法 开始 ======*/
+    private final Map<String, Object> attributes;
+
+    @Override
+    public Object getAttribute(String name) {
+        return attributes.get(name);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return Collections.enumeration(attributes.keySet());
+    }
+
+    @Override
+    public void setAttribute(String name, Object o) {
+        attributes.put(name, o);
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+        attributes.remove(name);
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return attributes.containsKey(DISPATCHER_TYPE) ? (DispatcherType) attributes.get(DISPATCHER_TYPE) : DispatcherType.REQUEST;
+    }
+
+    /*====== Request Attributes 相关方法 结束 ======*/
+
+
+
+    /*====== 异步 相关方法 开始 ======*/
+
+    @Override
+    public AsyncContext startAsync() {
+        return startAsync(this, null);
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) {
+        return ((NettyAsyncContext) getAsyncContext()).startAsync(servletRequest, servletResponse);
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return null != asyncContext && asyncContext.isAsyncStarted();
+    }
+
+    @SuppressWarnings("unused")
+    void setAsyncSupported(boolean asyncSupported) {
+        this.asyncSupported = asyncSupported;
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return asyncSupported;
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        if (null == asyncContext) {
+            asyncContext = new NettyAsyncContext(this, ctx);
+        }
+        return asyncContext;
+    }
+
+     /*====== 异步 相关方法 结束 ======*/
+
+    /*====== multipart/form-data 相关方法 开始 ======*/
+    @Override
+    public Collection<Part> getParts() throws IOException, IllegalStateException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Part getPart(String name) throws IOException, IllegalStateException, ServletException {
+        return null;
+    }
+
+     /*====== multipart/form-data 相关方法 结束 ======*/
+
+    @Override
+    public boolean isSecure() {
+        return getScheme().equalsIgnoreCase("HTTPS");
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return servletContext;
+    }
+
+    public ServletResponse getServletResponse() {
+        return servletResponse;
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return inputStream;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(inputStream, getCharacterEncoding()));
+    }
+
+    @Override
+    public int getContentLength() {
+        return inputStream.getCurrentLength();
+    }
+
+    @Override
+    public long getContentLengthLong() {
+        return (long) getContentLength();
+    }
+
+    private String characterEncoding;
+    @Override
+    public String getCharacterEncoding() {
+        if(characterEncoding == null) {
+            characterEncoding = parseCharacterEncoding();
+        }
+        return characterEncoding;
+    }
+
+    @Override
+    public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
+        characterEncoding = env;
+    }
+
+    @Override
+    public String getContentType() {
+        return headers.get("content-type");
+    }
+
+    private static final String DEFAULT_CHARSET = "UTF-8";
+
+    private String parseCharacterEncoding(){
+        String contentType = getContentType();
+        if (contentType == null) {
+            return DEFAULT_CHARSET;
+        }
+        int start = contentType.indexOf("charset=");
+        if (start < 0) {
+            return DEFAULT_CHARSET;
+        }
+        String encoding = contentType.substring(start + 8);
+        int end = encoding.indexOf(';');
+        if (end >= 0) {
+            encoding = encoding.substring(0, end);
+        }
+        encoding = encoding.trim();
+        if ((encoding.length() > 2) && (encoding.startsWith("\""))
+                && (encoding.endsWith("\""))) {
+            encoding = encoding.substring(1, encoding.length() - 1);
+        }
+        return encoding.trim();
+    }
+
+
+    /*====== 以下是暂不处理的接口方法 ======*/
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        return null;
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String path) {
+        return null;
+    }
+
+    @Override
+    public String getAuthType() {
+        return null;
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public void login(String username, String password) throws ServletException {
+    }
+
+    @Override
+    public void logout() throws ServletException {
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public String getPathTranslated() {
+        return null;
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        return false;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return null;
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return null;
+    }
+}
