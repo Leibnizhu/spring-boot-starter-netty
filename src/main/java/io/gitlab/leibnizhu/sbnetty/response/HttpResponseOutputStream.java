0175d4ce52edf3cbbb1e62f913f4a8ab27ccaf7e
Leibniz.Hu <leibnizhu@gmail.com>
2017-08-31 09:35:16 +0800

    增加注释，增加响应的content-length头

--- src/main/java/io/gitlab/leibnizhu/sbnetty/response/HttpResponseOutputStream.java
+++ src/main/java/io/gitlab/leibnizhu/sbnetty/response/HttpResponseOutputStream.java
@@ -17,21 +17,14 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 /**
- * A buffered {@link ServletOutputStream}, that writes Netty HTTP codec POJOs to the associated
- * {@link ChannelHandlerContext}.
- *
- *
+ * 响应输出流
  */
 public class HttpResponseOutputStream extends ServletOutputStream {
     private static final int DEFAULT_BUFFER_SIZE = 1024 * 8;
 
     private final ChannelHandlerContext ctx;
     private final NettyHttpServletResponse servletResponse;
-    private byte[] buf;
-    private int count;
-    private boolean closed;
-    private WriteListener writeListener;
-
+    private WriteListener writeListener; //监听器，暂时没处理
     HttpResponseOutputStream(ChannelHandlerContext ctx, NettyHttpServletResponse servletResponse) {
         this.ctx = ctx;
         this.servletResponse = servletResponse;
@@ -46,12 +39,22 @@ public class HttpResponseOutputStream extends ServletOutputStream {
     @Override
     public void setWriteListener(WriteListener writeListener) {
         checkNotNull(writeListener);
+        if(this.writeListener != null){
+            return; //只能设置一次
+        }
+        this.writeListener = writeListener;
         // TODO ISE when called more than once
         // TODO ISE when associated request is not async
     }
 
+    private byte[] buf; //缓冲区
+    private int count; //缓冲区游标（记录写到哪里）
+    private int totalLength;//内容总长度
+    private boolean closed; //是否已经调用close()方法关闭输出流
+
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
+        totalLength += len;
         if (len > count) {
             flushBuffer();
             ByteBuf content = ctx.alloc().buffer(len);
@@ -60,7 +63,7 @@ public class HttpResponseOutputStream extends ServletOutputStream {
             return;
         }
         writeBufferIfNeeded(len);
-        System.arraycopy(b, off, buf, count, len);
+        System.arraycopy(b, off, buf, count, len); //输入的b复制到缓存buf
         count += len;
     }
 
@@ -68,10 +71,11 @@ public class HttpResponseOutputStream extends ServletOutputStream {
     public void write(int b) throws IOException {
         writeBufferIfNeeded(1);
         buf[count++] = (byte) b;
+        totalLength++;
     }
 
     private void writeBufferIfNeeded(int len) throws IOException {
-        if (len > buf.length - count) {
+        if (len > buf.length - count) { //buffer剩余空间不足则flush
             flushBuffer();
         }
     }
@@ -88,10 +92,10 @@ public class HttpResponseOutputStream extends ServletOutputStream {
     private void flushBuffer(boolean lastContent) {
         if (count > 0) {
             ByteBuf content = ctx.alloc().buffer(count);
-            content.writeBytes(buf, 0, count);
-            count = 0;
+            content.writeBytes(buf, 0, count);//buf写入ByteBuf
+            count = 0;//游标归位
             writeContent(content, lastContent);
-        } else if (lastContent) {
+        } else if (lastContent) { //如果是最后一次flush，即便内容为空也要执行ctx.write写入EMPTY_LAST_CONTENT
             writeContent(Unpooled.EMPTY_BUFFER, true);
         }
     }
@@ -109,7 +113,7 @@ public class HttpResponseOutputStream extends ServletOutputStream {
             HttpResponse nettyResponse = servletResponse.getNettyResponse();
             ChannelFuture future = ctx.write(DefaultLastHttpContent.EMPTY_LAST_CONTENT);
             if (!HttpUtil.isKeepAlive(nettyResponse)) {
-                future.addListener(ChannelFutureListener.CLOSE);
+                future.addListener(ChannelFutureListener.CLOSE);//如果不是keep-alive，写完后关闭channel
             }
         }
     }
@@ -117,12 +121,9 @@ public class HttpResponseOutputStream extends ServletOutputStream {
     private void writeResponse(boolean lastContent) {
         HttpResponse response = servletResponse.getNettyResponse();
         // TODO implement exceptions required by http://tools.ietf.org/html/rfc2616#section-4.4
+        // 设置content-length头
         if (!HttpUtil.isContentLengthSet(response)) {
-            if (lastContent) {
-                HttpUtil.setContentLength(response, count);
-            } else {
-                HttpUtil.setTransferEncodingChunked(response,true);
-            }
+            HttpUtil.setContentLength(response, totalLength);
         }
         ctx.write(response, ctx.voidPromise());
     }
@@ -132,13 +133,13 @@ public class HttpResponseOutputStream extends ServletOutputStream {
         if (closed) {
             return;
         }
-        closed = true;
         try {
             flushBuffer(true);
             ctx.flush();
         } finally {
             buf = null;
         }
+        closed = true;
     }
 
     void resetBuffer() {
